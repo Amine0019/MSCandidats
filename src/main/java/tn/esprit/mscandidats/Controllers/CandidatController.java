9d0ae924d794bd93c464f8cdb54559c7d6e4d5f8
Mohamed Amine Larbi <larbi.mohamedamine@esprit.tn>
2026-01-28 17:18:37 +0100

    implementation du Controller Candidat avec toute les méthodes

--- src/main/java/tn/esprit/mscandidats/Controllers/CandidatController.java
+++ src/main/java/tn/esprit/mscandidats/Controllers/CandidatController.java
@@ -1,11 +1,15 @@
 package tn.esprit.mscandidats.Controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import tn.esprit.mscandidats.Entities.Candidat;
+import tn.esprit.mscandidats.Services.CandidatService;
 
 import javax.sql.DataSource;
+import java.util.List;
 
 @RestController
 @RequestMapping("/candidats")
@@ -25,6 +29,37 @@ public class CandidatController {
     public String getDbInfo() throws Exception {
         return dataSource.getConnection().getMetaData().getDatabaseProductName();
     }
+
+    @Autowired
+    private CandidatService candidatService;
+
+    @GetMapping
+    public ResponseEntity<List<Candidat>> getListCandid() {
+        List<Candidat> candidats = candidatService.getAll();
+        if (candidats.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(candidats);
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Candidat> createCandidat(@RequestBody Candidat candidat) {
+        return new ResponseEntity<>(candidatService.addCandidat(candidat), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Candidat> updateCandidat(@PathVariable("id") int id,
+                                                   @RequestBody Candidat candidat) {
+        return ResponseEntity.ok(candidatService.updateCandidat(id, candidat));
+    }
+
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteCandidat(@PathVariable("id") int id){
+        return new ResponseEntity<>(candidatService.deleteCandidat(id), HttpStatus.OK);
+    }
+
 
 
 }
