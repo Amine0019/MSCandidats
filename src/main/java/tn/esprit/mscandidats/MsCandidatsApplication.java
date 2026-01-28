31cfb6e6410f68bb38ed62c43c2dedfadecc9f4b
Mohamed Amine Larbi <larbi.mohamedamine@esprit.tn>
2026-01-28 16:27:28 +0100

    ApplicationRunner qui va permettre d'insérer automatiquement des candidats dans la base

--- src/main/java/tn/esprit/mscandidats/MsCandidatsApplication.java
+++ src/main/java/tn/esprit/mscandidats/MsCandidatsApplication.java
@@ -1,13 +1,36 @@
 package tn.esprit.mscandidats;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import tn.esprit.mscandidats.Entities.Candidat;
+import tn.esprit.mscandidats.Repositories.ICandidatRepository;
 
 @SpringBootApplication
 public class MsCandidatsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MsCandidatsApplication.class, args);
+    }
+    @Autowired
+    ICandidatRepository candidatRepository;
+
+    @Bean
+    ApplicationRunner init() {
+        return
+                (args) -> {
+                    // Vérifier si le repository est vide
+                    if (candidatRepository.count() == 0) {
+                        candidatRepository.save(new Candidat("Mariem", "Ch", "ma@esprit.tn"));
+                        candidatRepository.save(new Candidat("Sarra", "ab", "sa@esprit.tn"));
+                        candidatRepository.save(new Candidat("Mohamed", "ba", "mo@esprit.tn"));
+                        candidatRepository.save(new Candidat("Maroua", "dh", "maroua@esprit.tn"));
+                    }
+                    // Affichage
+                    candidatRepository.findAll().forEach(System.out::println);
+                };
     }
 
 }
