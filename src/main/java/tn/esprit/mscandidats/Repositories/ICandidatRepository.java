1d9283ac6bbe7e78bcb93ea7ce4def03c72a11d5
Mohamed Amine Larbi <larbi.mohamedamine@esprit.tn>
2026-01-28 16:44:39 +0100

    la méthode candidatByNom qui permet de recherche d’un candidat par son nom

--- src/main/java/tn/esprit/mscandidats/Repositories/ICandidatRepository.java
+++ src/main/java/tn/esprit/mscandidats/Repositories/ICandidatRepository.java
@@ -1,7 +1,13 @@
 package tn.esprit.mscandidats.Repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import tn.esprit.mscandidats.Entities.Candidat;
 
 public interface ICandidatRepository extends JpaRepository<Candidat, Integer> {
+    @Query("select c from Candidat c where c.nom like :name")
+    public Page<Candidat> candidatByNom(@Param("name") String n, Pageable pageable);
 }
