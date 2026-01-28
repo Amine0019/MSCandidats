fe72feec251fcb22369e0962bf7b5f68e585f79c
Mohamed Amine Larbi <larbi.mohamedamine@esprit.tn>
2026-01-28 15:59:13 +0100

    First Commit

---
+++ src/main/java/tn/esprit/mscandidats/MsCandidatsApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.mscandidats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCandidatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsCandidatsApplication.class, args);
+    }
+
+}
